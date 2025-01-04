733acac249ba62d5d5d15fd9e995cb174918345c
AAMIRR_ALI <amirwide@gmail.com>
2025-01-05 00:29:04 +0530

    update script

--- 15.sh
+++ 15.sh
@@ -11,43 +11,36 @@ VDT=$PWD/vendor/realme/bladerunner
 VCT=$PWD/vendor/realme/sm8250-common
 HD=$PWD/hardware/oplus
 KT=$PWD/kernel/realme/sm8250
+VX=$PWD/packages/apps/ViPER4AndroidFX
+Dolby=$PWD/hardware/dolby
 
 clone_trees() {
+echo -e "${blue}Extras Cloning ${clear}"
+git clone git@github.com:AAMIRR-ALI/vendor_extras.git vendor/extras
+
 echo -e "${blue}Device Tree Cloning ${clear}"
-git clone git@github.com:AAMIRR-ALI/android_device_realme_bladerunner.git -b vic device/realme/bladerunner
-git clone git@github.com:AAMIRR-ALI/android_device_realme_sm8250-common.git -b vic device/realme/sm8250-common
+git clone git@github.com:AAMIRR-ALI/android_device_realme_bladerunner.git -b rise-stable device/realme/bladerunner
+git clone git@github.com:AAMIRR-ALI/android_device_realme_sm8250-common.git -b rise-stable device/realme/sm8250-common
 
 echo -e "${blue}Vendor Tree Cloning${clear}"
 git clone https://github.com/Matrixx-Devices/vendor_realme_bladerunner.git -b udc vendor/realme/bladerunner
-git clone git@github.com:Matrixx-Devices/vendor_realme_sm8250-common.git -b vic vendor/realme/sm8250-common
-vendor/realme/sm8250-common
-
-echo -e "${blue}Kernel cloning ${clear}"
-git clone git@github.com:Matrixx-Devices/kernel_realme_sm8250.git kernel/realme/sm8250 --depth=1
+git clone git@github.com:RisingOSS-devices/vendor_realme_sm8250-common.git vendor/realme/sm8250-common --depth=1
 
 echo -e "${blue}Hardware cloning ${clear}"
-git clone git@github.com:Matrixx-Devices/hardware_oplus.git hardware/oplus -b cr
+git clone git@github.com:AAMIRR-ALI/hardware_oplus.git -b rise-stable hardware/oplus
+
+echo -e "${blue}Kernel cloning ${clear}"
+git clone git@github.com:AAMIRR-ALI/bigshot_kernel_realme_sm8250.git kernel/realme/sm8250
 
 echo -e "${blue}Dolby cloning ${clear}"
-git clone git@github.com:AAMIRR-ALI/hardware_dolby.git hardware/dolby
+git clone git@github.com:AAMIRR-ALI/old_dolby.git hardware/dolby
 
 echo -e "${blue}ViperX cloning ${clear}"
 git clone https://github.com/Chaitanyakm/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
-echo -e "${blue}VancedManager cloning ${clear}"
-git clone https://github.com/AAMIRR-ALI/VancedManager.git packages/apps/VancedManager
-
-echo -e "${blue}BCR cloning ${clear}"
-git clone https://github.com/Chaitanyakm/vendor_bcr.git vendor/bcr
-
-rm -rf hardware/qcom-caf/sm8250/audio
-git clone git@github.com:ALISPROJECTSS/hardware_qcom-caf_sm8250_audio.git hardware/qcom-caf/sm8250/audio
-
-rm -rf hardware/qcom-caf/sm8250/display
-git clone git@github.com:ALISPROJECTSS/hardware_qcom-caf_sm8250_display.git hardware/qcom-caf/sm8250/display
 }
 
-if [ -d $DT ] && [ -d $CT ] && [ -d $VDT ] && [ -d $VCT ] && [ -d $KT ] && [ -d $HD ] ; then
+if [ -d $DT ] && [ -d $CT ] && [ -d $VDT ] && [ -d $VCT ] && [ -d $KT ] && [ -d $HD ] && [ -d $VX ] && [ -d $Dolby ] ; then
     echo -e "${blue}Trees exists, Skipping !${clear}"
 else
     clone_trees
@@ -55,4 +48,4 @@ fi
 
 . build/envsetup.sh
 
-lunch aosp_bladerunner-ap3a-userdebug && export CCACHE_EXEC=$(which ccache) && export USE_CCACHE=1 && export CCACHE_DIR=/home/ali/.ccache/ && ccache -M 1000G
+riseup bladerunner && rise b
